46a08268724eabab575fe10fc0234fca79c68b84
huynhtruc0309 <huynhtruc0309@gmail.com>
2021-06-07 01:39:31 +0700

    first init

---
+++ modules/multi_template_matching/multi_template_matching.py
@@ -0,0 +1,19 @@
+import MTM
+from MTM import matchTemplates, drawBoxesOnRGB
+from pathlib import Path
+import cv2
+import json
+import numpy as np
+
+import utils
+
+class MultiTemplateMatching(object):
+    def __init__(self, templates, N_object, method, maxOverlap):
+        super(MultiTemplateMatching, self).__init__()
+        self.templates
+
+    def load_templates():
+
+        
+    def __call__(self, image):
+        return image
